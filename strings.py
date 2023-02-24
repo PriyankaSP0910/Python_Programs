e0e957b4a890c788ff9c2c70bc41e50043559005
Priyanka SP <85819116+PriyankaSP0910@users.noreply.github.com>
2023-02-24 16:51:28 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,37 @@
+while True:
+    print("==========main menu==================")
+    print("1.String operations\n 2.Tuple operations\n 3.exit")
+    print("=====================================")
+    a=int(input("select option"))
+    if a==1:
+       str1=input("enter the string 1")
+       str2=input("enter the string 2")
+       while True:
+           print("====================================")
+           print("1.length\n2.uppercase \n3.lowercase\n4.reverse\n5.title\n6.count\n7.find\n8.min\n9.concat\n10.exit")
+           print("====================================")
+           x=int(input("enter your choice"))
+           if x==1:
+              print(len(str1))
+           elif x==2:
+              print(str1.upper())
+           elif x==3:
+              print(str1.lower())
+           elif x==4:
+              print(str1[::-1])
+           elif x==5:
+              print(str1.title())
+           elif x==6:
+              print(str1.count("a"))
+           elif x==7:
+              print(str1.find("hello"))
+           elif x==8:
+              print(min(str1))
+           elif x==9:
+              print(str1+str2)
+           else:
+              break
+    elif a==2:
+         print("work under progress")
+    else:
+       exit()
