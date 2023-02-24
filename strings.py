7071df04d5dc4ffc7c17523852313415283e5a44
Priyanka SP <85819116+PriyankaSP0910@users.noreply.github.com>
2023-02-24 16:54:36 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -2,15 +2,15 @@ while True:
     print("==========main menu==================")
     print("1.String operations\n 2.exit")
     print("=====================================")
-    a=int(input("select option"))
+    a=int(input("select option: "))
     if a==1:
-       str1=input("enter the string 1")
-       str2=input("enter the string 2")
+       str1=input("enter the string 1: ")
+       str2=input("enter the string 2: ")
        while True:
            print("====================================")
            print("1.length\n2.uppercase \n3.lowercase\n4.reverse\n5.title\n6.count\n7.find\n8.min\n9.concat\n10.exit")
            print("====================================")
-           x=int(input("enter your choice"))
+           x=int(input("enter your choice: "))
            if x==1:
               print(len(str1))
            elif x==2:
